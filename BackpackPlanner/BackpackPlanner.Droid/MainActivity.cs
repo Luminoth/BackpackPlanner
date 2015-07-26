a2ac8bfcf5d43101e247192ba69be3fd8d329b32
Shane Lillie <ignignokterr@gmail.com>
2015-07-26 08:59:51 -0700

    Initial import (now without Xamarin.Forms!)

---
+++ BackpackPlanner/BackpackPlanner.Droid/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace EnergonSoftware.BackpackPlanner.Droid
+{
+	[Activity (Label = "Backpacking Planner", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.myButton);
+			
+			button.Click += delegate {
+				button.Text = string.Format ("{0} clicks!", count++);
+			};
+		}
+	}
+}
+
+
