1009ad1f0c0c428de9d490c776071376aa2d306b
Shane Lillie <ignignokterr@gmail.com>
2015-07-25 23:38:42 -0700

    Initial import

---
+++ BackpackPlanner/BackpackPlanner/BackpackPlanner.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,15 @@
+﻿using EnergonSoftware.BackpackPlanner.WinPhone.Resources;
+
+namespace EnergonSoftware.BackpackPlanner.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources
+        { get { return _localizedResources; } }
+    }
+}
---
+++ BackpackPlanner/BackpackPlanner/BackpackPlanner.WinPhone/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Phone.Controls;
+
+namespace EnergonSoftware.BackpackPlanner.WinPhone
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            SupportedOrientations = SupportedPageOrientation.PortraitOrLandscape;
+
+            Xamarin.Forms.Forms.Init();
+            LoadApplication(new BackpackPlanner.App());
+        }
+    }
+}
