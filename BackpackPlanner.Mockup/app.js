e46ae0d11e2cb8ae612cb3abbe5d76450c57fc71
Shane Lillie <ignignokterr@gmail.com>
2015-07-27 16:59:27 -0700

    Adding a mockup (functional wireframes) project

---
+++ BackpackPlanner.Mockup/app.js
@@ -0,0 +1,16 @@
+﻿var mockupApp = angular.module("mockupApp", [
+    "ngRoute",
+    "ngMaterial",
+    "ui.bootstrap"
+]);
+
+mockupApp.config(["$routeProvider",
+    function($routeProvider) {
+        $routeProvider.when("/index", {
+            templateUrl: "partials/main.html"
+        })
+        .otherwise({
+            redirectTo: "/index"
+        });
+    }
+]);
