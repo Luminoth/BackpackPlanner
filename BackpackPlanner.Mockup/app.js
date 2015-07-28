4045df4deaeb05f4ea009c30fef882513389b46f
Shane Lillie <ignignokterr@gmail.com>
2015-07-27 18:34:07 -0700

    Working on the mockup UI

--- BackpackPlanner.Mockup/app.js
+++ BackpackPlanner.Mockup/app.js
@@ -1,16 +1,36 @@
 ﻿var mockupApp = angular.module("mockupApp", [
     "ngRoute",
     "ngMaterial",
+    "ngMdIcons",
     "ui.bootstrap"
 ]);
 
 mockupApp.config(["$routeProvider",
     function($routeProvider) {
         $routeProvider.when("/index", {
-            templateUrl: "partials/main.html"
+            templateUrl: "partials/main.html",
+            title: "Backpacking Planner"
+        })
+        .when("/gear/manage", {
+            templateUrl: "partials/gear/manage.html",
+            title: "Manage Gear"
+        })
+        .when("/gear/items", {
+            templateUrl: "partials/gear/items.html",
+            title: "Gear Items"
         })
         .otherwise({
             redirectTo: "/index"
         });
+    }
+]);
+
+mockupApp.run(["$rootScope",
+    function($rootScope) {
+        $rootScope.$on("$routeChangeSuccess",
+            function(event, currentRoute, previousRoute) {
+                $rootScope.title = currentRoute.title;
+            }
+        );
     }
 ]);
