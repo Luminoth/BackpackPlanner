0aa500dd9d44d9c5e558affc213dd059bf07e2be
Shane Lillie <ignignokterr@gmail.com>
2015-07-30 19:56:26 -0700

    Sidenav now collapses when a link is clicked

--- BackpackPlanner.Mockup/scripts/controllers.js
+++ BackpackPlanner.Mockup/scripts/controllers.js
@@ -1,7 +1,14 @@
 ﻿var mockupControllers = angular.module("mockupControllers", []);
 
-mockupControllers.controller("AppCtrl", ["$scope", "$mdSidenav",
-    function($scope, $mdSidenav) {
-        $scope.toggleSidenav = function() { $mdSidenav("left").toggle(); }
+mockupControllers.controller("AppCtrl", ["$scope", "$location", "$mdSidenav",
+    function($scope, $location, $mdSidenav) {
+        $scope.isActive = function(viewLocation) {
+            // set the nav item as active when we're looking at its location
+            return $location.path() === viewLocation;
+        }
+
+        $scope.toggleSidenav = function() {
+            $mdSidenav("left").toggle();
+        }
     }
 ]);
