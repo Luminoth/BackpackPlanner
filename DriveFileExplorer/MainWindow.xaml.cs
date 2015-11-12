70dd5e26e378c122003c34809e8783a71862a5eb
Shane Lillie <ignignokterr@gmail.com>
2015-11-12 07:24:56 -0800

    NuGet package updates Adding google drive packages to the file explorer Adding some basic controls to the file explorer

--- DriveFileExplorer/MainWindow.xaml.cs
+++ DriveFileExplorer/MainWindow.xaml.cs
@@ -1,10 +1,17 @@
-﻿namespace EnergonSoftware.BackpackPlanner.DriveFileExplorer
+﻿using System.Windows;
+
+namespace EnergonSoftware.BackpackPlanner.DriveFileExplorer
 {
     public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MenuFileExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
