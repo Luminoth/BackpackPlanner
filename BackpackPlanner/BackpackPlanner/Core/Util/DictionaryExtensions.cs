5f6ba8a9e6387369e512d626af118757e58efcf9
Shane Lillie <ignignokterr@gmail.com>
2017-01-31 08:09:12 -0800

    Added Dictionary extension helpers

---
+++ BackpackPlanner/BackpackPlanner/Core/Util/DictionaryExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace EnergonSoftware.BackpackPlanner.Core.Util
+{
+    public static class DictionaryExtensions
+    {
+        public static V GetOrDefault<K, V>(this Dictionary<K, V> dictionary, K key, V defaultValue=default(V))
+        {
+            V value;
+            return dictionary.TryGetValue(key, out value) ? value : defaultValue;
+        }
+
+        public static V GetAndRemove<K, V>(this Dictionary<K, V> dictionary, K key, V defaultValue=default(V))
+        {
+            V value;
+            if(!dictionary.TryGetValue(key, out value)) {
+                return defaultValue;
+            }
+
+            dictionary.Remove(key);
+            return value;
+        }
+    }
+}
