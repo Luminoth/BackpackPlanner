81cbd7e724bab5c3961bbbccc8fc3057cf891140
Shane Lillie <ignignokterr@gmail.com>
2017-03-30 18:23:12 -0700

    Added ViewGearSystem functionality

--- BackpackPlanner/BackpackPlanner.Droid/Fragments/Gear/Systems/ViewGearSystemFragment.cs
+++ BackpackPlanner/BackpackPlanner.Droid/Fragments/Gear/Systems/ViewGearSystemFragment.cs
@@ -14,15 +14,30 @@
    limitations under the License.
 */
 
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Android.App;
 using Android.OS;
 using Android.Views;
+using Android.Widget;
 
+using EnergonSoftware.BackpackPlanner.Core.Logging;
+using EnergonSoftware.BackpackPlanner.DAL;
+using EnergonSoftware.BackpackPlanner.DAL.Models.Gear.Items;
 using EnergonSoftware.BackpackPlanner.DAL.Models.Gear.Systems;
+using EnergonSoftware.BackpackPlanner.Droid.Adapters.Gear;
+using EnergonSoftware.BackpackPlanner.Droid.Util;
+
+using Microsoft.EntityFrameworkCore;
 
 namespace EnergonSoftware.BackpackPlanner.Droid.Fragments.Gear.Systems
 {
     public sealed class ViewGearSystemFragment : ViewItemFragment<GearSystem>
     {
+        private static readonly ILogger Logger = CustomLogger.GetLogger(typeof(ViewGearSystemFragment));
+
         protected override int LayoutResource => Resource.Layout.fragment_view_gear_system;
 
         protected override int TitleResource => Resource.String.title_view_gear_system;
@@ -35,7 +50,23 @@ namespace EnergonSoftware.BackpackPlanner.Droid.Fragments.Gear.Systems
 
 #region Controls
         private Android.Support.Design.Widget.TextInputLayout _gearSystemNameEditText;
+
+        private TextView _noGearItemsTextView;
+        private TextView _noGearItemsAddedTextView;
+        private ListView _gearItemsListView;
+        private Android.Support.Design.Widget.FloatingActionButton _addGearItemButton;
+
         private Android.Support.Design.Widget.TextInputLayout _gearSystemNoteEditText;
+#endregion
+
+#region Gear Items
+        private GearItem[] _gearItems;
+
+        private string[] _gearItemsNames;
+
+        private bool[] _selectedGearItems;
+
+        private GearItemEntryListAdapter _gearItemListAdapter;
 #endregion
 
         public override void OnViewCreated(View view, Bundle savedInstanceState)
@@ -47,11 +78,120 @@ namespace EnergonSoftware.BackpackPlanner.Droid.Fragments.Gear.Systems
 
             _gearSystemNoteEditText = view.FindViewById<Android.Support.Design.Widget.TextInputLayout>(Resource.Id.view_gear_system_note);
             _gearSystemNoteEditText.EditText.Text = Item.Note;
+
+            _noGearItemsTextView = View.FindViewById<TextView>(Resource.Id.no_gear_items);
+            _noGearItemsAddedTextView = View.FindViewById<TextView>(Resource.Id.no_gear_items_added);
+
+            _gearItemListAdapter = new GearItemEntryListAdapter(this);
+
+            _gearItemsListView = View.FindViewById<ListView>(Resource.Id.gear_items_list);
+            _gearItemsListView.Adapter = _gearItemListAdapter;
+
+            _addGearItemButton = view.FindViewById<Android.Support.Design.Widget.FloatingActionButton>(Resource.Id.fab_add_gear_item);
+            _addGearItemButton.Click += AddGearItemButtonClickEventHandler;
+        }
+
+        public override void OnResume()
+        {
+            base.OnResume();
+
+            ProgressDialog progressDialog = DialogUtil.ShowProgressDialog(Activity, Resource.String.label_loading_items, false, true);
+
+            Task.Run(async () =>
+                {
+                    using(DatabaseContext dbContext = BaseActivity.BackpackPlannerState.DatabaseState.CreateContext()) {
+                        _gearItems = await dbContext.GearItems.ToArrayAsync().ConfigureAwait(false);
+                        _gearItemsNames = (from x in _gearItems select x.Name).ToArray();
+                        _selectedGearItems = new bool[_gearItems.Length];
+                    }
+
+                    OnItemsLoaded(progressDialog);
+                }
+            );
+        }
+
+        private void OnItemsLoaded(ProgressDialog progressDialog)
+        {
+            if(null == _gearItems) {
+                return;
+            }
+
+            Activity.RunOnUiThread(() =>
+            {
+                SetGearItemList();
+
+                progressDialog.Dismiss();
+
+                UpdateView();
+            });
+        }
+
+        private void AddGearItemButtonClickEventHandler(object sender, EventArgs args)
+        {
+            DialogUtil.ShowMultiChoiceAlertWithSearch(Activity, Resource.String.label_add_gear_items, _gearItemsNames, _selectedGearItems,
+                (a, b) =>
+                {
+                    _gearItemListAdapter.Filter.InvokeFilter(b.NewText, new FilterListener<GearItemEntry>(_gearItemListAdapter));
+                },
+                (a, b) =>
+                {
+                    UpdateGearItemList(b.Which, b.IsChecked);
+                }
+            );
+        }
+
+        private void SetGearItemList()
+        {
+            for(int i=0; i<_gearItems.Length; ++i) {
+                GearItem gearItem = _gearItems[i];
+
+                GearItemEntry gearItemEntry = Item.GearItems.FirstOrDefault(x => x.ModelId == gearItem.Id);
+                if(null != gearItemEntry) {
+                    _selectedGearItems[i] = true;
+                    _gearItemListAdapter.AddItem(gearItemEntry);
+                }
+            }
+
+            // TODO: this may be unnecessary
+            _gearItemListAdapter.NotifyDataSetChanged();
+        }
+
+        private void UpdateGearItemList(int index, bool isSelected)
+        {
+            _selectedGearItems[index] = isSelected;
+
+            GearItem gearItem = _gearItems[index];
+            if(isSelected) {
+                GearItemEntry gearItemEntry = new GearItemEntry(gearItem)
+                {
+                    Count = 1
+                };
+                _gearItemListAdapter.AddItem(gearItemEntry);
+            } else {
+                _gearItemListAdapter.RemoveItem(gearItem);
+            }
+
+            // TODO: this may be unnecessary
+            _gearItemListAdapter.NotifyDataSetChanged();
+
+            UpdateView();
+        }
+
+        private void UpdateView()
+        {
+            bool hasGearItems = null != _gearItems && _gearItems.Any();
+            bool hasGearItemsAdded = _gearItemListAdapter.Count > 0;
+
+            _noGearItemsTextView.Visibility = hasGearItems ? ViewStates.Gone : ViewStates.Visible;
+            _noGearItemsAddedTextView.Visibility =  hasGearItemsAdded ? ViewStates.Gone : ViewStates.Visible;
+            _gearItemsListView.Visibility = hasGearItemsAdded ? ViewStates.Visible : ViewStates.Gone;
+            _addGearItemButton.Visibility = hasGearItems ? ViewStates.Visible : ViewStates.Gone;
         }
 
         protected override void OnDoDataExchange()
         {
             Item.Name = _gearSystemNameEditText.EditText.Text;
+            Item.AddGearItems(_gearItemListAdapter.Items);
             Item.Note = _gearSystemNoteEditText.EditText.Text;
         }
 
