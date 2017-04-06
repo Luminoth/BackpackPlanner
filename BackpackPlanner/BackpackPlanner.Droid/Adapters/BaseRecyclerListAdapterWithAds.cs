e9477057a96bc87eb7d221e476187af1342e58ef
Shane Lillie <ignignokterr@gmail.com>
2017-04-06 11:38:21 -0700

    Fixed ad layout not being inflated correctly
    Now using the actual ad unit id
    Fixed inserting ads into the list of items

--- BackpackPlanner/BackpackPlanner.Droid/Adapters/BaseRecyclerListAdapterWithAds.cs
+++ BackpackPlanner/BackpackPlanner.Droid/Adapters/BaseRecyclerListAdapterWithAds.cs
@@ -19,12 +19,15 @@ using System.Collections.Generic;
 using Android.Gms.Ads;
 using Android.Views;
 
+using EnergonSoftware.BackpackPlanner.Core.Logging;
 using EnergonSoftware.BackpackPlanner.Droid.Fragments;
 
 namespace EnergonSoftware.BackpackPlanner.Droid.Adapters
 {
     public abstract class BaseRecyclerListAdapterWithAds<T> : BaseRecyclerListAdapter<T> where T: class
     {
+        private static readonly ILogger Logger = CustomLogger.GetLogger(typeof(BaseRecyclerListAdapterWithAds<T>));
+
 #if DEBUG
         private const int AdFrequency = 2;
 #else
@@ -32,8 +35,6 @@ namespace EnergonSoftware.BackpackPlanner.Droid.Adapters
 #endif
 
         protected const int AdViewType = 1;
-
-        private const int AdLayoutResource = Resource.Id.view_ad;
 
         private sealed class AdViewHolder : BaseViewHolder
         {
@@ -48,7 +49,7 @@ namespace EnergonSoftware.BackpackPlanner.Droid.Adapters
             switch(viewType)
             {
             case AdViewType:
-                View itemView = LayoutInflater.From(parent.Context).Inflate(AdLayoutResource, parent, false);
+                View itemView = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.view_ad, parent, false);
                 return new AdViewHolder(itemView, this);
             case ListItemViewType:
             default:
@@ -89,7 +90,11 @@ namespace EnergonSoftware.BackpackPlanner.Droid.Adapters
         {
             NativeExpressAdView adView = new NativeExpressAdView(Fragment.Context)
             {
-                //AdUnitId = "ca-app-pub-TODO"
+            #if USE_REAL_ADS
+                AdUnitId = Fragment.GetString(Resource.String.native_ad_unit_id)
+            #else
+                AdUnitId = Fragment.GetString(Resource.String.test_native_ad_unit_id)
+            #endif
             };
 
             // NOTE: this works because we create the view *after*
@@ -97,20 +102,32 @@ namespace EnergonSoftware.BackpackPlanner.Droid.Adapters
             float density = Fragment.Resources.DisplayMetrics.Density;
             adView.AdSize = new AdSize((int)(Fragment.Layout.Width / density), 150);
 
-            adView.LoadAd(new AdRequest.Builder().Build());
+            AdRequest.Builder builder = new AdRequest.Builder();
+            TestDevices.AddTestDevices(builder);
+            TestDevices.SetGender(Fragment.BaseActivity.BackpackPlannerState.PersonalInformation, builder);
+            AdRequest adRequest = builder.Build();
+
+            Logger.Debug($"Loading ad, is test device: {adRequest.IsTestDevice(Fragment.Context)}");
+            adView.LoadAd(adRequest);
 
             return adView;
         }
 
+        private bool IsAdPosition(int position)
+        {
+            return 0 != position && 0 == (position + 1) % AdFrequency;
+        }
+
         public override int GetItemViewType(int position)
         {
-            return 0 == position % AdFrequency ? AdViewType : ListItemViewType;
+            bool isAd = IsAdPosition(position);
+            return isAd ? AdViewType : base.GetItemViewType(position);
         }
 
         public override void SetItems(IReadOnlyCollection<T> items)
         {
             var newItemList = new List<T>(items);
-            for(int i=0; i<items.Count; i += AdFrequency) {
+            for(int i=AdFrequency-1; i<newItemList.Count; i+=AdFrequency) {
                 newItemList.Insert(i, null);
             }
             base.SetItems(newItemList);
